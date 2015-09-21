b119bb08ec6b839393321877d5c2847afa9bef67
Jan van Haarst <jan.vanhaarst@wur.nl>
2015-09-21 14:32:11 +0200

    Added NG50, and prepared for multiple plots with different minimal contig length.

--- AssemblyStats.py
+++ AssemblyStats.py
@@ -10,14 +10,17 @@ import itertools
 # Global variables
 max_contigs = 0000  # Set to zero to ignore
 min_length = 200  # Set to zero to ignore
+# Set the expected genome size in order to calculate the NG50, if zero, ignore
+expected_genome_size = 0 # 0.350e9
+
 DPI = 300
 assemblies = {}
 outfile = "assemblies_%i.png" % max_contigs  # Leave empty if you want to use the interactive output instead of a file.
 #outfile=''
 
 # Colors to cycle through
-# http://colorbrewer2.org/?type=qualitative&scheme=Paired&n=12
-# http://stackoverflow.com/questions/12236566/setting-different-color-for-each-series-in-scatter-plot-on-matplotlib
+# Adapted from http://colorbrewer2.org/?type=qualitative&scheme=Paired&n=12
+# Usage from http://stackoverflow.com/questions/12236566/setting-different-color-for-each-series-in-scatter-plot-on-matplotlib
 colors = itertools.cycle([
     '#1f78b4',
     '#e31a1c',
@@ -45,34 +48,40 @@ def csv2string(data):
 # Class definitions
 class Assembly:
     """Class to hold the assembly information, and perform the calculations"""
-    def __init__(self, name, filename):
+    def __init__(self, name, filename,min_length):
         # from __future__ import division
         from itertools import islice
         from Bio import SeqIO
         import sys
         self.name = name
         self.filename = filename
+        self.min_length=min_length
         self.sizes = []
         sys.stderr.write("Reading in %s\n" % self.filename)
         # For each contig/scaffold, calculate length, and sort large to small
         for rec in SeqIO.parse(open(self.filename), "fasta"):
-            if (len(rec) >= min_length):
+            if (len(rec) >= self.min_length):
                 self.sizes.append(len(rec))
             else:
-                if (min_length == 0):
+                if (self.min_length == 0):
                     self.sizes.append(len(rec))
         self.sizes.sort(reverse=True)
         # Calculate all the statistics we can already do
         self.total_length = sum(self.sizes)
         self.count = len(self.sizes)
         self.sum = self.total_length
-        self.max = max(self.sizes)
-        self.min = min(self.sizes)
+        try:
+            self.max = max(self.sizes)
+            self.min = min(self.sizes)
+        except: # there is no contig smaller than maximum size
+            raise
         self.average = sum(self.sizes) / len(self.sizes)
         self.median = self.sizes[int(len(self.sizes) / 2)]
         # Store the first number on the size list as the first of the incremental list
         self.incremental_sizes = []
         self.incremental_sizes.append(self.sizes[0])
+        self.NG50 = 0
+        self.LG50 = 0
         self.N50 = 0
         self.L50 = 0
         self.N90 = 0
@@ -83,7 +92,15 @@ class Assembly:
         # Now iterate over the sizes list, and incrementally add it to the incremental list
         for index, size in enumerate(islice(self.sizes, 1, None)):
             self.incremental_sizes.append(self.incremental_sizes[-1] + size)
-            # While we iterate over the array, we can gather N50, L50 when we reach half of the total assembly
+            # While we iterate over the array, we can gather NG50, N50, LG50 and the L50 when we reach half of the total assembly
+            if (expected_genome_size != 0):
+                if (self.incremental_sizes[-1] > expected_genome_size / 2) and (self.NG50 == 0):
+                    self.NG50 = size
+                    # We need to add one to the index because we started to loop at 1, not 0
+                    self.LG50 = index + 1
+            else:
+                self.NG50 = 0
+                self.LG50 = 0
             if (self.incremental_sizes[-1] > self.total_length / 2) and (self.N50 == 0):
                 self.N50 = size
                 # We need to add one to the index because we started to loop at 1, not 0
@@ -111,6 +128,8 @@ class Assembly:
         line.append(self.median)
         line.append(self.N50)
         line.append(self.L50)
+        line.append(self.NG50)
+        line.append(self.LG50)
         line.append(self.N90)
         line.append(self.L90)
         line.append(self.N95)
@@ -132,6 +151,8 @@ class Assembly:
         print "Median:%s" % format(self.median, "n")
         print "N50:%s" % format(self.N50, "n")
         print "L50:%s" % format(self.L50, "n")
+        print "NG50:%s" % format(self.NG50, "n")
+        print "LG50:%s" % format(self.LG50, "n")
         print "N90:%s" % format(self.N90, "n")
         print "L90:%s" % format(self.L90, "n")
         print "N95:%s" % format(self.N95, "n")
@@ -150,7 +171,7 @@ if (len(sys.argv) == 2):
         first_line = f.readline()
     if (first_line[0] == '>'):  # We got a single FASTA file
         input_file = sys.argv[1]
-        assemblies[input_file] = Assembly(input_file, input_file)
+        assemblies[input_file] = Assembly(input_file, input_file,min_length)
     else:  # We assume a valid CSV file, put the values in the assemblies dict
         file_name = sys.argv[1]
 	fp = open(file_name, 'rb')
@@ -158,14 +179,14 @@ if (len(sys.argv) == 2):
         for row in csv_file:
             input_name = row['Seq_Name']
             input_file = row['Seq_File']
-            assemblies[input_name] = Assembly(input_name, input_file)
+            assemblies[input_name] = Assembly(input_name, input_file,min_length)
 if (len(sys.argv) > 2):
     # Multiple files, for now assume they are all valid FASTA
     for input_file in sys.argv[1:]:
-        assemblies[input_file] = Assembly(input_file, input_file)
+        assemblies[input_file] = Assembly(input_file, input_file,min_length)
 
 # Now plot the A50 plots and print the stats
-print csv2string(["Name","Count","Sum","Max","Min","Average","Median","N50","L50","N90","L90","N95","L95","Count > 1000"])
+print csv2string(["Name","Count","Sum","Max","Min","Average","Median","N50","L50","NG50","LG50","N90","L90","N95","L95","Count > 1000"])
 for name, assembly in iter(sorted(assemblies.items())):
     print(csv2string(assembly.return_stats()))
     line = pylab.plot(assembly.incremental_sizes, label=name,color=next(colors))
